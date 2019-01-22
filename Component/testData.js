e7b547d4b2c257792b464fbfe5d1f16852851812
hoodie Coder <mayank4kathuria@gmail.com>
2019-01-22 18:00:17 +0530

    Test data for testing purposes

---
+++ Component/testData.js
@@ -0,0 +1,19 @@
+// Collection of dummy data for various functionalities for testing the App
+
+export const accounts = [
+    { username: "username", password: "password" },
+    { username: "mayank", password: "test" },
+    { username: "maku@123", password: "somebodyIusedToKnow" },
+    { username: "username@something.com", password: "test" },
+    { username: "usernameBot", password: "#test@" },
+    { username: "foo", password: "bar" },
+    { username: "bar", password: "bar" },
+    { username: "baz", password: "foo-bar" },
+    { username: "wtf", password: "fuck" },
+    { username: "number10", password: "101010"},
+]
+
+
+
+
+
